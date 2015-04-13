6b0e86268a56b87e34a2b1aece986c0196b6cc1d
muyun <wenlzhao@gmail.com>
2015-04-13 23:14:00 +0800

    fix the prime err

--- randomized_protocol/msgSend.m
+++ randomized_protocol/msgSend.m
@@ -11,10 +11,20 @@ function  [s, p] = msgSend(x, m)
 
 %  the len_x bits of x in R1
 len_x = length(x); 
+% bug here if the len_x is 1 bit
+if len_x == 1,
+    len_x = 2;
+end
+
 %  choose m uniformly random primes here
-p = randi([2, len_x^2],  1,  m);
+%p = randi([2, len_x^2],  1,  m);
+all_ps = primes(len_x^2);
+%len_ps = length(all_ps);
+pick = randi([2, length(all_ps)], 1, m);  %get the uniformly random indexes
+p = all_ps(pick);
 
 % si = Number(x) mod pi
+% x must represent a nonnegative integer value smaller than or equal to 2^52
 s =  mod (bin2dec(x),  p) ;
 
 % for the binary representations (not the string representations here) in the message
